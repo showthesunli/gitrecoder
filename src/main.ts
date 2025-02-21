7783fe51eed2149bbc0e85a9bf4733486e9dcb33
showthesunli <showthesunli@gmail.com>
2025-02-21 23:25:09 +0800

    feat(canvas): 添加基础图形支持
    - 使用Fabric.js创建Canvas实例
    - 新增可配置的矩形和圆形组件
    - 设置元素定位及样式属性

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,34 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import { Canvas, Rect, Circle } from "fabric";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas = new Canvas("c");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const rect = new Rect({
+  left: 250,
+  top: 250,
+  fill: "red",
+  width: 500,
+  height: 500,
+  originX: "center",
+  originY: "center",
+});
+
+canvas.add(rect);
+
+rect.set({
+  left: "300",
+});
+
+rect.setCoords();
+canvas.renderAll();
+
+const circle = new Circle({
+  left: 250,
+  top: 250,
+  fill: "blue",
+  radius: 250,
+  originX: "center",
+  originY: "center",
+});
+
+canvas.add(circle);
